86e7d0f955bfa14e39ca3dcc75e1c7512019c673
silver-hornet <70822422+silver-hornet@users.noreply.github.com>
2020-12-14 08:43:52 -0500

    Added asteroid and randomized asteroid movement

---
+++ Assets/Scripts/AsteroidController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AsteroidController : MonoBehaviour
+{
+    // References
+    Rigidbody2D asteroidRB;
+
+    // Config
+    float moveSpeedX;
+    float moveSpeedY;
+    [SerializeField] float minSpeed;
+    [SerializeField] float maxSpeed;
+
+    void Start()
+    {
+        asteroidRB = GetComponent<Rigidbody2D>();
+        moveSpeedX = Random.Range(minSpeed, maxSpeed) * Mathf.Sign(Random.Range(-1f, 1f));
+        moveSpeedY = Random.Range(minSpeed, maxSpeed) * Mathf.Sign(Random.Range(-1f, 1f));
+    }
+
+    void Update()
+    {
+        
+        asteroidRB.velocity = new Vector3(moveSpeedX, moveSpeedY, 0); // Use velocity, instead of something like AddForce, to ensure the same, constant speed
+    }
+}
